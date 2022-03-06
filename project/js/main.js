a4a571520c1c1daa60102f74bd1ca6b7d6b0a1df
Paul Atlan <paul.atlan@experconnect.com>
2022-03-06 16:29:02 +0000

    ouh la la

--- project/js/main.js
+++ project/js/main.js
@@ -1,9 +1,20 @@
-function showPicture(){
-  // use jQuery ($ is shorthand) to find the div on the page and then change the html
-  // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
-  $("#image").append('<img class="rounded-circle" src="images/high-five.gif"/>');
-  $("p").html("High five! You're building your first web app!");
+// function showPicture(){
+//   // use jQuery ($ is shorthand) to find the div on the page and then change the html
+//   // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
+//   $("#image").append('<img class="rounded-circle" src="images/high-five.gif"/>');
+//   $("p").html("High five! You're building your first web app!");
+// 
+//   // jQuery can do a lot of crazy stuff, so make sure to Google around to find out more
+//   
+// }
 
-  // jQuery can do a lot of crazy stuff, so make sure to Google around to find out more
-  
+$(document).ready(function(){
+  getWeather();
+})
+
+function geatweather(){
+    var url = "https://api.openweathermap.org/data/2.5/weather?q=Paris&appid="+apiKey;
+    $.ajax(url,{success:function(data){
+      console.log(data);
+    }})
 }
