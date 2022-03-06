d8eddc2af46751eac03af85ad64585a9121baefd
Paul Atlan <paul.atlan@experconnect.com>
2022-03-06 15:05:28 +0000

    corrected path

--- project/start.js
+++ project/start.js
@@ -13,11 +13,11 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
-app.get('/', function(request, response) {
+app.get('/about', function(request, response) {
   response.render('pages/about');
 });
 
-app.get('/', function(request, response) {
+app.get('/projects', function(request, response) {
   response.render('pages/projects');
 });
 
