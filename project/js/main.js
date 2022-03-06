ce2a5c04c27ddc3e12ca9629e1222920535e0f3f
Paul Atlan <paul.atlan@experconnect.com>
2022-03-06 17:56:40 +0000

    api stuff

--- project/js/main.js
+++ project/js/main.js
@@ -10,10 +10,12 @@
 
 $(document).ready(function() {
   getWeather("Paris");
+  countUsers();
 })
 
 function getWeather(searchQuery) {
-  var url = "https://api.openweathermap.org/data/2.5/weather?q=" + searchQuery + "&units=metric&appid=" + apiKey;
+  var url = "https://api.openweathermap.org/data/2.5/weather?q=" + searchQuery + "&units=metric&appid=85b143711beff5ce8af1d98cca6078f5";
+   // + apiKeyWeather;
   $(".oups").text("");
   $.ajax(url, {
     success: function(data) {
@@ -23,12 +25,34 @@ function getWeather(searchQuery) {
       $(".feels").text(data.main.feels_like);
     },
     error: function(data) {
+      console.log(data);
       $(".oups").text("An Error Occurred");
     }
   })
 }
 
-function searchWeather() {
-  var searchQuery = $(".search").val();
-  getWeather(searchQuery)
+function countUsers(){
+  var url = "https://apihswebapp@apihswebapp.fr:Namiki01@expert.experconnect.test.demedicis.fr/api/users/count";
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url);
+  xhr.setRequestHeader("accept", "application/json");
+  xhr.setRequestHeader("apiKey", apiKey);
+  xhr.setRequestHeader("token", token);
+  xhr.onreadystatechange = function () {
+     if (xhr.readyState === 4) {
+       
+        console.log(xhr.status);
+        console.log(xhr.responseText);
+        $(".users").text(data.name);
+        
+     }};
+  xhr.send();
+  
+  
+  
 }
+
+// function searchWeather() {
+//   var searchQuery = $(".search").val();
+//   getWeather(searchQuery)
+// }
